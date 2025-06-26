ad59bec3fb95ad0c74a9e7a41bf3112a036de6b1
Andy Kammerer <ankammer@f0r5s1.clusters.42paris.fr>
2025-06-26 12:56:39 +0200

    cpp05

--- CPP05/ex03/srcs/main.cpp
+++ CPP05/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 16:27:36 by ankammer          #+#    #+#             */
-/*   Updated: 2025/06/25 18:35:02 by ankammer         ###   ########.fr       */
+/*   Updated: 2025/06/26 12:40:32 by ankammer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,66 +32,55 @@ int main()
         Bureaucrat jack("jack", 1);
         std::cout << std::endl;
 
-        std::cout << "--------------------------------------------------------------" << std::endl;
-        std::cout << "                                                             |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+        std::cout << "                                                                         |" << std::endl;
 
         robot = someRandomIntern.makeForm("robotomy request", "Bender");
         std::cout << std::endl;
-        if (robot)
-        {
-            jack.signForm(*robot);
-            jack.executeForm(*robot);
-        }
-        std::cout << "                                                             |" << std::endl;
-        std::cout << "--------------------------------------------------------------" << std::endl;
+        jack.signForm(*robot);
+        jack.executeForm(*robot);
+        std::cout << "                                                                         |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+
         std::cout << std::endl;
         std::cout << std::endl;
         std::cout << std::endl;
 
-        std::cout << "--------------------------------------------------------------" << std::endl;
-        std::cout << "                                                             |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+        std::cout << "                                                                         |" << std::endl;
 
         pardon = someRandomIntern.makeForm("presidential pardon", "Jones");
         std::cout << std::endl;
-        if (pardon)
-        {
-            jack.signForm(*pardon);
-            jack.executeForm(*pardon);
-        }
-        std::cout << "                                                             |" << std::endl;
-        std::cout << "--------------------------------------------------------------" << std::endl;
+        jack.signForm(*pardon);
+        jack.executeForm(*pardon);
+        std::cout << "                                                                         |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+
         std::cout << std::endl;
         std::cout << std::endl;
         std::cout << std::endl;
 
-        std::cout << "--------------------------------------------------------------" << std::endl;
-        std::cout << "                                                             |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+        std::cout << "                                                                         |" << std::endl;
 
         shrub = someRandomIntern.makeForm("shrubbery creation", "arbuste");
         std::cout << std::endl;
-        if (shrub)
-        {
-            jack.signForm(*shrub);
-            jack.executeForm(*shrub);
-        }
-        std::cout << "                                                             |" << std::endl;
-        std::cout << "--------------------------------------------------------------" << std::endl;
+        jack.signForm(*shrub);
+        jack.executeForm(*shrub);
+        std::cout << "                                                                         |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+
         std::cout << std::endl;
         std::cout << std::endl;
         std::cout << std::endl;
 
-        std::cout << "--------------------------------------------------------------" << std::endl;
-        std::cout << "                                                             |" << std::endl;
+        std::cout << "--------------------------------------------------------------------------" << std::endl;
+        std::cout << "                                                                         |" << std::endl;
 
         invalid = someRandomIntern.makeForm("invalid form", "arbuste");
         std::cout << std::endl;
-        if (invalid)
-        {
-            jack.signForm(*invalid);
-            jack.executeForm(*invalid);
-        }
-        std::cout << "                                                             |" << std::endl;
-        std::cout << "--------------------------------------------------------------" << std::endl;
+        jack.signForm(*invalid);
+        jack.executeForm(*invalid);
         std::cout << std::endl;
         std::cout << std::endl;
         std::cout << std::endl;
@@ -100,7 +89,9 @@ int main()
     {
         std::cerr << "Error catched: " << e.what() << std::endl;
     }
-    
+    std::cout << "                                                                         |" << std::endl;
+    std::cout << "--------------------------------------------------------------------------" << std::endl;
+
     std::cout << std::endl;
     delete robot;
     std::cout << std::endl;
