610edda56417047084aa144831a85e7cf73ebc5f
Andy Kammerer <ankammer@f2r6s1.clusters.42paris.fr>
2025-07-14 14:12:58 +0200

    cpp07

---
+++ CPP07/ex00/header/smmax.tpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   smmax.tpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 13:59:37 by ankammer          #+#    #+#             */
+/*   Updated: 2025/07/14 14:09:49 by ankammer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SMMAX_TEMPLATE_TPP
+#define SMMAX_TEMPLATE_TPP
+#include <iostream>
+
+template<typename T>
+void swap(T & x, T & y)
+{
+    T tmp = x;
+    x = y;
+    y = tmp;    
+}
+
+template<typename T>
+const T & min(const T & x, const T & y)
+{
+    return(x <= y ? x : y);
+}
+template<typename T>
+const T & max(const T & x, const T & y)
+{
+    return(x >= y ? x : y);
+}
+#endif
---
+++ CPP07/ex00/srcs/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 14:08:40 by ankammer          #+#    #+#             */
+/*   Updated: 2025/07/14 14:12:22 by ankammer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/smmax.tpp"
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}
